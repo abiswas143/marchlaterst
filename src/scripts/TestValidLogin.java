cd557b2cfa782896f1cc11d463a6e359ec4aebe5
ajit <biswasajit1432@gmail.com>
2019-04-14 09:27:06 +0530

    log4j updated

--- src/scripts/TestValidLogin.java
+++ src/scripts/TestValidLogin.java
@@ -1,4 +1,6 @@
 package scripts;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -9,7 +11,10 @@ import pompages.LoginPage;
 public class TestValidLogin extends BaseTest {
 	@Test
 	public void testValidLogin(){
+		Logger log = LogManager.getLogger(TestValidLogin.class.getName());
+		log.debug("creating an object of loginpage pom class");
 		LoginPage lp = new LoginPage(driver);
+		log.info("object of loginpage pom class is created successfully...");
 		String username = Lib.getCellValue("ValidLogin", 1, 0);
 		String password = Lib.getCellValue("ValidLogin", 1, 1);
 		//enter username
